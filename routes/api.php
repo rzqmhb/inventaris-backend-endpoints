0451f3ad3561449eef79137e7894725b6502420e
rzqmhb <roziqmahbubi22@gmail.com>
2024-08-08 09:42:17 +0700

    feat: add api route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\InventarisController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::group(['prefix'=> 'user'], function () {
+        Route::get('/profile', [UserController::class, 'getProfile'])->name('profile.get');
+        Route::post('/profile/update', [UserController::class, 'updateProfile'])->name('profile.update');
+    });
+
+    Route::group(['prefix'=> 'inventaris'], function () {
+        Route::get('/all', [InventarisController::class, 'getAll'])->name('inventaris.getAll');
+        Route::get('/get/{id}', [InventarisController::class, 'getById'])->name('inventaris.get');
+        Route::post('/add', [InventarisController::class, 'add'])->name('inventaris.add');
+        Route::post('/update/{id}', [InventarisController::class, 'update'])->name('inventaris.update');
+        Route::post('/delete/{id}', [InventarisController::class, 'delete'])->name('inventaris.delete');
+    });
+});
+
